ec3cb7c564b37818c72a35ed8d5fd9d970e90637
Tera Schaller <tschaller722@gmail.com>
2017-01-13 11:49:01 -0600

    editing code (#1)
    tried editing some code cloned from git hub on my local desktop git in
    atom

--- average.cpp
+++ average.cpp
@@ -19,7 +19,7 @@ int main() {
     //initialize variables
     double userNum1, userNum2, userNum3, userNum4, userNum5;
     double average;
-    
+
     //prompt user to enter 5 numbers
     //store user numbers as variables
     cout << "Please enter 5 numbers." << endl;
@@ -28,12 +28,14 @@ int main() {
     cin >> userNum3;
     cin >> userNum4;
     cin >> userNum5;
-    
+
     //calculate the average and print answer
     average = (userNum1 + userNum2 + userNum3 + userNum4 + userNum5)/5.0;
-    
+
     cout << "The average of those numbers is:" << endl;
     cout << average << endl;
-    
+
     return 0;
 }
+
+//added this in git(desktop)->atom on 1/13
