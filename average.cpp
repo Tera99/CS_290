69649720efa84f2763220f36898e4a67974663a5
Tera Schaller <tschaller722@gmail.com>
2017-01-13 11:23:45 -0600

    assignment 1
    Adding two files (simple C++ code as examples) to a repository with OSU-CS290-Tester as a collaborator.

---
+++ average.cpp
@@ -0,0 +1,39 @@
+
+
+/*********************************************************************
+ ** Author:Terezie Schaller
+ ** Date:10-2-15
+ ** Description:Week2, project a, average of 5 numbers
+ *********************************************************************/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**************
+ **Description: calculates the average of 5 numbers
+ **entered by the user
+ ****************/
+
+int main() {
+    //initialize variables
+    double userNum1, userNum2, userNum3, userNum4, userNum5;
+    double average;
+    
+    //prompt user to enter 5 numbers
+    //store user numbers as variables
+    cout << "Please enter 5 numbers." << endl;
+    cin >> userNum1;
+    cin >> userNum2;
+    cin >> userNum3;
+    cin >> userNum4;
+    cin >> userNum5;
+    
+    //calculate the average and print answer
+    average = (userNum1 + userNum2 + userNum3 + userNum4 + userNum5)/5.0;
+    
+    cout << "The average of those numbers is:" << endl;
+    cout << average << endl;
+    
+    return 0;
+}
---
+++ program2_7.cpp
@@ -0,0 +1,27 @@
+//
+//  program2_7.cpp
+//
+//
+//  Created by Terezie Schaller on 9/30/15.
+//
+//
+// Simple program with a variable
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int numberTwo;
+
+    number = 5;
+    cout << "The value of the number is " << "number" << endl;
+    cout << "The value of the number is " << number << endl;
+
+    numberTwo = 7;
+    cout << "The value of the other number is " << numberTwo << endl;
+
+    return 0;
+
+}
